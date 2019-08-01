d64ea6fbdbec589edf4cf2e69ae6af7e6516b6dd
evelycafe <evelysany@gmail.com>
2019-08-01 19:57:25 -0300

    Versao 1 - adição do arquivo exemplo1.php

---
+++ exemplo1.php
@@ -0,0 +1,32 @@
+<?php
+
+/* variaveis primitivas*/
+    //variavel do tipo int
+    $var_inteira = 45;
+    //variavel float
+    $var_ponto_flutuante = 3.14;
+    //variavel string de caracteres
+    $var_string = "Uma string";
+    //variavel booleana
+    $var_boolean = true;
+    //variavel nula
+    $var_null = null;
+/**********************************/
+
+/*variaveis compostas*/
+//vetores e matrizes
+/*tipos de dados compostos heterogeneos associativos*/
+
+$v = array("a","b",10,true,"xyz",3.89);
+
+print_r($v);
+
+$m[0][0]="valor da posicao 0x0";
+$m[0][1]="valor da posicao 0x1";
+$m[1][0]="valor da posicao 1x0";
+$m[1][1]="valor da posicao 1x1";
+/**************************************************/
+
+
+
+?>
